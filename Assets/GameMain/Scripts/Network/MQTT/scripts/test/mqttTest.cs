a9c7ae162f0010022fd6651594c372fc14eedd01
g7020176 <2421146195@qq.com>
2023-07-31 11:12:01 +0800

    MQTT协议接收数据的脚本上传，mqttTest.cs是测试脚本

---
+++ Assets/GameMain/Scripts/Network/MQTT/scripts/test/mqttTest.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt.Utility;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+
+using System;
+
+public class mqttTest : MonoBehaviour {
+    private MqttClient client;
+    public string ip = "127.0.0.1";
+    public int port = 1883;
+    public string username = "admin";
+    public string password = "g7020176";
+    private string lastReceivedMessage = ""; // 上次获取的消息
+
+    // Use this for initialization
+    void Start() {
+        // create client instance 
+        client = new MqttClient(IPAddress.Parse(ip), port, false, null);
+        // register to message received 
+        client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+        string clientId = Guid.NewGuid().ToString();
+
+        client.Connect(clientId, username, password);
+
+        // subscribe to the topic with QoS 2 
+        
+    }
+
+    void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) {
+        //Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message));
+        CheckAndUpdateMessage(System.Text.Encoding.UTF8.GetString(e.Message));
+    }
+
+    void Update() {
+        client.Subscribe(new string[] { "robot_command" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+    }
+
+    void CheckAndUpdateMessage(string newMessage) {
+        if (newMessage != lastReceivedMessage) {
+            // 获取最新消息的逻辑操作
+            Debug.Log("Getting the latest message: " + newMessage);
+            // ...
+            lastReceivedMessage = newMessage; // 更新上次获取的消息
+        }
+    }
+}
