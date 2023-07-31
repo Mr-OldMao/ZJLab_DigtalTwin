99148d97cd807abb6d2183bc45ff6ef8dc9b6ceb
g7020176 <2421146195@qq.com>
2023-07-31 15:19:35 +0800

    mqtt的收发数据，已测试

--- Assets/GameMain/Scripts/Test/TestMqtt.cs
+++ Assets/GameMain/Scripts/Test/TestMqtt.cs
@@ -15,23 +15,33 @@ using System;
 /// </summary>
 public class TestMqtt : MonoBehaviour {
     private MqttClient client;
-    public string ip = "127.0.0.1";
-    public int port = 1883;
-    public string username = "admin";
-    public string password = "g7020176";
+    public string ip = "127.0.0.1";//服务器IP
+    public int port = 8083;//服务器端口
+    public string username = "admin";//服务器端账号
+    public string password = "g7020176";//服务器端密码
     private string lastReceivedMessage = ""; // 上次获取的消息
+    public string clientId = "Unity Mqtt Client";//客户端ID
 
-    // Use this for initialization
+    private string topic1 = "robot1"; // 替换为你要订阅的主题名称
+
+    //初始化MqttClient实例，连接，订阅
     void Start() {
         // create client instance 
         client = new MqttClient(IPAddress.Parse(ip), port, false, null);
         // register to message received 
         client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
-        string clientId = Guid.NewGuid().ToString();
 
-        client.Connect(clientId, username, password);
+        
+        //clientId = Guid.NewGuid().ToString();
+        ConnectToMQTT();
+       
+        
+        Subscribenewtopic(topic1);
+        string message = "test,robot,mqtt"; // 替换为你要订阅的主题名称
+        PublishMessage(topic1,message);
+    
 
-        // subscribe to the topic with QoS 2 
+
         
     }
 
@@ -40,8 +50,43 @@ public class TestMqtt : MonoBehaviour {
         CheckAndUpdateMessage(System.Text.Encoding.UTF8.GetString(e.Message));
     }
 
+    void ConnectToMQTT(){//连接到mqtt客户端
+        try
+        {
+            if (!client.IsConnected)
+            {
+                client.Connect(clientId, username, password);
+                Debug.Log("Connected to MQTT Broker!");
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("MQTT Connection Error: " + e.Message);
+        }
+    }
+
+    void Subscribenewtopic(string topic){//订阅新主题
+        client.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+    }
+
+    void PublishMessage(string topic, string message) {
+        byte[] payload = System.Text.Encoding.UTF8.GetBytes(message);
+        client.Publish(topic, payload, MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+    }
+
+
     void Update() {
-        client.Subscribe(new string[] { "robot_command" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+  
+
+        if (!client.IsConnected)//保持连接
+        {
+            ConnectToMQTT(); 
+        }
+
+        client.Subscribe(new string[] { topic1 }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+       
+
     }
 
     void CheckAndUpdateMessage(string newMessage) {
